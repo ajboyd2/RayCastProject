ca95b9a2429b9b4e4fb37c752a4e0096f2d061a8
ajboyd2 <ajboyd@calpoly.edu>
2016-09-10 18:18:25 -0700

    Adjusted test jig to support scenes and ambient lighting.

--- RayCastProject/RayCastProject/TestJig.cpp
+++ RayCastProject/RayCastProject/TestJig.cpp
@@ -13,13 +13,15 @@ int main()
   
   std::vector<Sphere> SphereList;
 
-  SphereList.push_back(Sphere(Point(-1, 2, 0), 1, cv::Vec3b(100, 100, 40)));
-  SphereList.push_back(Sphere(Point(1, 2, 1), 0.8, cv::Vec3b(40, 180, 40)));
-  SphereList.push_back(Sphere(Point(0, 2, -1), 1.2, cv::Vec3b(0, 100, 200)));
+  SphereList.push_back(Sphere(Point(-1, 2, 0), 1, cv::Vec3b(100, 100, 40), Finish(0.8)));
+  SphereList.push_back(Sphere(Point(1, 2, 1), 0.8, cv::Vec3b(40, 180, 40), Finish(0.4)));
+  SphereList.push_back(Sphere(Point(0, 2, -1), 1.2, cv::Vec3b(0, 100, 200), Finish(0.5)));
+
+  Scene MasterScene(SphereList, Point(0, 0, 100), cv::Vec3b(255, 255, 255));
 
   Camera Cam(Ray(Point(0, 0, 0), Vector(0, 1, 0)), Vector(0, 0, 1), 2, 3, 3, cv::Size(1280, 1280));
 
-  Cam.Render(SphereList);
+  Cam.Render(MasterScene);
 
   cv::namedWindow("Test");
   cv::imshow("Test", Cam.GetImage());
